3c61aa0579736eb88df2c937236b1c6610935ec5
AlexisElizabeth <alexiselizabethpaluch@gmail.com>
2022-12-24 13:50:02 +0100

    Commit 2

--- TicTacToe.py
+++ TicTacToe.py
@@ -7,8 +7,8 @@ class TicTacToe:
     def __init__(self):
         self.board = []
         self.space = " - "
-        self.x = " X "
-        self.o = " 0 "
+        self.player = " X "
+        self.computer = " 0 "
 
     def build_table(self):
         for i in range(3):
@@ -23,6 +23,15 @@ class TicTacToe:
                 print(space, end=" ")
             print()
 
+    def choose_symbol(self):
+        player = input("Would you like to be X or O? > ").lower()
+        if player == "x":
+            self.player = " X "
+            self.computer = " O "
+        else:
+            self.player = " O "
+            self.computer = " X "
+
     def human_move(self):
         turn_ongoing = True
         while turn_ongoing:
@@ -31,9 +40,9 @@ class TicTacToe:
             row_index = int(move[1]) - 1
             try:
                 if self.board[row_index][column_index] == self.space:
-                    self.board[row_index][column_index] = self.x
+                    self.board[row_index][column_index] = self.player
                     self.print_table()
-                    self.search_for_win(self.x)
+                    self.search_for_win(self.player)
                     turn_ongoing = False
                 else:
                     print("That space is taken. Select again.")
@@ -48,18 +57,18 @@ class TicTacToe:
             random_row = randint(0, 2)
             random_column = randint(0, 2)
             if self.board[random_row][random_column] == self.space:
-                self.board[random_row][random_column] = self.o
+                self.board[random_row][random_column] = self.computer
                 self.print_table()
-                self.search_for_win(self.o)
+                self.search_for_win(self.computer)
                 turn_ongoing = False
 
-    def search_for_win(self, player):
+    def search_for_win(self, active_player):
         # search for win in rows
 
         for row in self.board:
             win = True
             for character in row:
-                if character != player:
+                if character != active_player:
                     win = False
                     break
             if win:
@@ -69,7 +78,7 @@ class TicTacToe:
         for i in range(3):
             win = True
             for j in range(3):
-                if self.board[j][i] != player:
+                if self.board[j][i] != active_player:
                     win = False
                     break
             if win:
@@ -78,7 +87,7 @@ class TicTacToe:
         # search for win in diagonal 1:
         win = True
         for k in range(3):
-            if self.board[k][k] != player:
+            if self.board[k][k] != active_player:
                 win = False
                 break
         if win:
@@ -87,7 +96,7 @@ class TicTacToe:
         # search for win in diagonal 2:
         win = True
         for i in range(0, 3):
-            if self.board[i][2 - i] != player:
+            if self.board[i][2 - i] != active_player:
                 win = False
                 break
         if win:
@@ -101,3 +110,42 @@ class TicTacToe:
                     full = False
                     break
         return full
+
+    def start_game(self):
+        self.build_table()
+        self.print_table()
+        self.choose_symbol()
+
+    def player_order(self):
+        print("Randomly picking a player to start the game")
+        first = randint(1, 2)
+        time.sleep(2)
+        if first == 1:
+            print("The human player starts")
+            return self.player, self.computer
+        else:
+            print("The computer starts")
+            return self.computer, self.player
+
+    def game_ended(self, active_player):
+        if self.search_for_win(active_player):
+            print(f"{active_player} wins!")
+            return True
+        elif self.board_full():
+            print("Board full!")
+            return True
+        return False
+
+    def play_game(self):
+        self.start_game()
+        player_order = self.player_order()
+
+        while not self.game_ended(self.player) or self.game_ended(self.computer):
+            self.human_move()
+            if self.game_ended(self.player):
+                break
+            self.computer_move()
+            if self.game_ended(self.computer):
+                break
+
+        print("Game over!")
--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from TicTacToe import TicTacToe
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     game = TicTacToe()
-    game.build_table()
-    game.print_table()
+    game.play_game()
 
-    while not game.board_full():
-        while not game.search_for_win(game.x) or game.search_for_win(game.o):
-            game.human_move()
-            if game.search_for_win(game.x):
-                print(f"{game.x} wins!")
-                break
-            game.computer_move()
-            if game.search_for_win(game.o):
-                print(f"{game.o} wins!")
-                break
 
-    print("Game over!")
+
+
